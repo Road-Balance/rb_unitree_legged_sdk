6474dee1e853951077d9b9bba8e399fe6a467966
kimsooyoung <tge1375@naver.com>
2021-11-28 23:36:42 +0900

    Feature : new mode examples added

---
+++ new_examples/backflip_test.cpp
@@ -0,0 +1,100 @@
+/*****************************************************************
+ Copyright (c) 2020, Unitree Robotics.Co.Ltd. All rights reserved.
+******************************************************************/
+
+#include "unitree_legged_sdk/unitree_legged_sdk.h"
+#include <math.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+
+using namespace UNITREE_LEGGED_SDK;
+
+class Custom
+{
+public:
+    Custom(uint8_t level): safe(LeggedType::A1), udp(8090, "192.168.123.161", 8082, sizeof(HighCmd), sizeof(HighState)){
+        udp.InitCmdData(cmd);
+    }
+    void UDPRecv();
+    void UDPSend();
+    void RobotControl();
+
+    Safety safe;
+    UDP udp;
+    HighCmd cmd = {0};
+    HighState state = {0};
+    int motiontime = 0;
+    float dt = 0.002;     // 0.001~0.01
+};
+
+
+void Custom::UDPRecv()
+{
+    udp.Recv();
+}
+
+void Custom::UDPSend()
+{  
+    udp.Send();
+}
+
+void Custom::RobotControl() 
+{
+    motiontime += 2;
+    udp.GetRecv(state);
+
+    // printf("%f %f %f %f %f\n", state.imu.rpy[1], state.imu.rpy[2], state.position[0], state.position[1], state.velocity[0]);
+
+    cmd.mode = 0;
+    cmd.gaitType = 0;
+    cmd.speedLevel = 0;
+    cmd.footRaiseHeight = 0;
+    cmd.bodyHeight = 0;
+    cmd.euler[0]  = 0;
+    cmd.euler[1] = 0;
+    cmd.euler[2] = 0;
+    cmd.velocity[0] = 0.0f;
+    cmd.velocity[1] = 0.0f;
+    cmd.yawSpeed = 0.0f;
+
+    if(motiontime > 0 && motiontime < 1000){
+        cmd.mode = 2;
+    }
+    if(motiontime > 1000 && motiontime < 2000){
+        cmd.mode = 1;
+    }
+    if(motiontime > 2000 && motiontime < 4500){
+        cmd.mode = 9;
+    }
+
+    if(motiontime> 4500){
+        cmd.mode = 1;
+    }
+
+    udp.SetSend(cmd);
+}
+
+int main(void) 
+{
+    std::cout << "Communication level is set to HIGH-level." << std::endl
+              << "WARNING: Make sure the robot is standing on the ground." << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
+
+    Custom custom(HIGHLEVEL);
+    // InitEnvironment();
+    LoopFunc loop_control("control_loop", custom.dt,    boost::bind(&Custom::RobotControl, &custom));
+    LoopFunc loop_udpSend("udp_send",     custom.dt, 3, boost::bind(&Custom::UDPSend,      &custom));
+    LoopFunc loop_udpRecv("udp_recv",     custom.dt, 3, boost::bind(&Custom::UDPRecv,      &custom));
+
+    loop_udpSend.start();
+    loop_udpRecv.start();
+    loop_control.start();
+
+    while(1){
+        sleep(10);
+    };
+
+    return 0; 
+}
